ede6639859717224dfe4704b6c7f0ffd6b9ff6b4
Aubrey Lavigne <Aubrey.Lavigne@therigy.com>
2019-07-13 01:19:21 -0400

    Implement Concurrency

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"runtime"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
+const batchSize = 1000000
+
 func main() {
-	for i := 10; i < 1000000000; i++ {
-		num := proposed(i)
-		if num.isCranky() {
-			fmt.Println(num)
-		}
+
+	wg = sync.WaitGroup{}
+
+	// need to implement the counter to make see how many ongoing
+	// processes we have
+
+	wg.Add(runtime.NumCPU())
+	in := make(chan int64)
+	out := make(chan int64)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func(in chan int64, out chan int64) {
+			for start := range in {
+				for i := start; i < start+batchSize; i++ {
+					num := proposed(i)
+					if num.isCranky() {
+						log.Print(num)
+						// out <- int64(num)
+					}
+				}
+			}
+
+			wg.Done()
+		}(in, out)
 	}
+
+	for i := 0; i < 100000000; i++ {
+		in <- int64(i * batchSize)
+	}
+
+	close(in)
+
+	wg.Wait()
 }
